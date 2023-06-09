cf79cf35266864c695382c1ff0ccc5440b5e1a7f
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-06-09 15:51:58 +0200

    [DOCS] test

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+            tasks: [
+                {
+                    text:'sciamm a mangia na pezz',
+                    done: false
+                },
+                {
+                    text:'sciamm a mangia na pezz',
+                    done: false
+                },
+                {
+                    text:'sciamm a mangia na pezz',
+                    done: false
+                },
+                {
+                    text:'sciamm a mangia na pezz',
+                    done: false
+                },
+                {
+                    text:'sciamm a mangia na pezz',
+                    done: false
+                },
+                {
+                    text:'sciamm a mangia na pezz',
+                    done: false
+                },
+                {
+                    text:'sciamm a mangia na pezz',
+                    done: false
+                },
+                {
+                    text:'sciamm a mangia na pezz',
+                    done: false
+                },
+                {
+                    text:'sciamm a mangia na pezz',
+                    done: false
+                },
+            ]
+
+        }
+    }
+}).mount('.container')
